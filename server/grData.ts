ebbd2f6d668fb3e8952d8ebfda6941c01373d168
TMEMark <markobuljan.os@gmail.com>
2021-09-20 14:44:15 +0200

    Created grocery data in mongoose.

--- server/grData.ts
+++ server/grData.ts
@@ -1,36 +1,34 @@
 const mongoose = require("mongoose");
+import api from "express";
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:3000/groceries");
+  await mongoose.connect("mongodb://localhost:27017/groceries");
 }
 
-const grocerySchema = mongoose.Schema({
-  _id: {
-    type: Number,
-  },
-  name: {
-    type: String,
-  },
-  quantity: {
-    type: Number,
-  },
-  price: {
-    type: Number,
-  },
+const grocery = mongoose.model("grocery", {
+  _id: { type: Number },
+  name: { type: String },
+  quantity: { type: Number },
+  price: { type: Number },
 });
 
-const grocery = mongoose.model("grocery", grocerySchema);
-
-const groceries = [
+const groceryArray = [
   { _id: 1, name: "Milk", quantity: 2, price: 5.99 },
   { _id: 2, name: "Beer", quantity: 6, price: 11 },
   { _id: 3, name: "Watermelon", quantity: 1, price: 38.8 },
 ];
 
-grocery.insertMany(groceries, function (error, docs) {
-  if (error) {
-    return console.error(error);
-  } else {
-    console.log(groceries);
-  }
+groceryArray.forEach(function (n) {
+  grocery.findOneAndUpdate(n, n, { upsert: true }, function (err, doc) {
+    console.log(doc);
+  });
+});
+
+api.get("/grocery", function (req, res) {
+  res.send("hello world");
 });
